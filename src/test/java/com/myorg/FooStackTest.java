ade38ad5d38cce1d70d655eea81f2cb982692e6d
Sean C. Sullivan <github@seansullivan.com>
2021-05-01 19:00:52 -0700

    AWS CDK v1 -> v2

--- src/test/java/com/myorg/FooStackTest.java
+++ src/test/java/com/myorg/FooStackTest.java
@@ -1,6 +1,6 @@
 package com.myorg;
 
-import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.App;
 import software.amazon.awscdk.cxapi.CloudAssembly;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
